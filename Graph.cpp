deba7cb84099905c17530e65300040aab4b5a048
solidw <solidw@naver.com>
2018-11-29 19:47:44 +0900

    graph!

--- Graph.cpp
+++ Graph.cpp
@@ -1,5 +1,6 @@
 #include "Graph.h"
 #include <string.h>
+#include "Passenger.h"
 
 
 Graph::Graph(int vn) :vn(vn)
@@ -18,6 +19,8 @@ Graph::Graph(int vn) :vn(vn)
 
 }
 
+
+
 Graph::~Graph(void)
 
 {
@@ -34,6 +37,134 @@ Graph::~Graph(void)
 
 }
 
+Graph* Graph::makeGraph(string addr) {
+
+
+	try {
+		fstream inFile;
+		inFile.open(addr);	//파일 입력
+
+		string _size;
+
+		string _v1;
+		string _v2;
+		string _dist;
+		string _trash;
+		getline(inFile, _size, ',');	//size 입력
+
+		int size = atoi(_size.c_str());
+		if (typeid(size).name() != typeid(int).name()) throw(0);
+		Graph* _graph = new Graph(size);
+		//Graph* _graph = new Graph(atoi(_size.c_str()));	//사이즈만큼 그래프 생성
+
+
+		while (!inFile.eof()) {
+
+
+			getline(inFile, _trash, '\n');
+
+			getline(inFile, _v1, ',');
+			if (typeid(_v1).name() != typeid(string).name()) throw(1);
+
+			getline(inFile, _v2, ',');
+			if (typeid(_v2).name() != typeid(string).name()) throw(1);
+
+			getline(inFile, _dist, ',');
+			if (typeid(_dist).name() != typeid(string).name()) throw(1);
+
+			_graph->AddEdge(_v1, _v2, atoi(_dist.c_str()));
+
+
+		}
+		return _graph;
+	}
+
+	catch (int error) {
+		if (error == 0) printf("사이즈 입력이 올바르지 않습니다.\n");
+		else if (error == 1) printf("그래프 정점 형식이 올바르지 않습니다.\n");
+	}
+}
+
+/*
+int Graph::findMinWay(int start, Passenger* ps , Passenger* ps1) {
+	int arr[2];
+	int dest1 = convertEdge(ps->getDestAddr());
+	int dest2 = convertEdge(ps1->getDestAddr());
+
+	arr[0] = matrix[start][dest1] + matrix[dest1][dest2];
+	arr[1] = matrix[start][dest2] + matrix[dest2][dest1];
+
+}
+*/
+
+
+int Graph::findMinWay(int start, int dest1, int dest2) {
+	int arr[2];
+	arr[0] = matrix[start][dest1] + matrix[dest1][dest2];
+	arr[1] = matrix[start][dest2] + matrix[dest2][dest1];
+
+	int index = findMin(arr, sizeof(arr) / sizeof(int));
+
+	printf("최소 거리 : %d \n", arr[index]);
+	return index;
+}
+
+
+
+int Graph::findMinWay(int start, int dest1, int dest2, int dest3) {
+	int arr[6];
+	arr[0] = matrix[start][dest1] + matrix[dest1][dest2] + matrix[dest2][dest3];
+	arr[1] = matrix[start][dest1] + matrix[dest1][dest3] + matrix[dest3][dest2];
+	arr[2] = matrix[start][dest2] + matrix[dest2][dest3] + matrix[dest3][dest1];
+	arr[3] = matrix[start][dest2] + matrix[dest2][dest1] + matrix[dest1][dest3];
+	arr[4] = matrix[start][dest3] + matrix[dest3][dest1] + matrix[dest1][dest2];
+	arr[5] = matrix[start][dest3] + matrix[dest3][dest2] + matrix[dest2][dest1];
+
+
+	int index = findMin(arr, sizeof(arr) / sizeof(int));
+
+	printf("최소 거리 : %d \n", arr[index]);
+	return index;
+}
+
+int Graph::findMinWay(int start, int dest1, int dest2, int dest3, int dest4) {
+	int arr[24];
+	arr[0] = matrix[start][dest1] + matrix[dest1][dest2] + matrix[dest2][dest3] + matrix[dest3][dest4];//1234
+	arr[1] = matrix[start][dest1] + matrix[dest1][dest2] + matrix[dest2][dest4] + matrix[dest4][dest3];//1243
+	arr[2] = matrix[start][dest1] + matrix[dest1][dest3] + matrix[dest3][dest2] + matrix[dest2][dest4];//1324
+	arr[3] = matrix[start][dest1] + matrix[dest1][dest3] + matrix[dest3][dest4] + matrix[dest4][dest2];//1342
+	arr[4] = matrix[start][dest1] + matrix[dest1][dest4] + matrix[dest4][dest2] + matrix[dest2][dest3];	//1423
+	arr[5] = matrix[start][dest1] + matrix[dest1][dest4] + matrix[dest4][dest3] + matrix[dest3][dest3];	//1432
+																										//---여기까지 1먼저
+	arr[6] = matrix[start][dest2] + matrix[dest2][dest1] + matrix[dest1][dest3] + matrix[dest3][dest4];	//2134
+	arr[7] = matrix[start][dest2] + matrix[dest2][dest1] + matrix[dest1][dest4] + matrix[dest4][dest3];	//2143
+	arr[8] = matrix[start][dest2] + matrix[dest2][dest3] + matrix[dest3][dest1] + matrix[dest1][dest4];	//2314
+	arr[9] = matrix[start][dest2] + matrix[dest2][dest3] + matrix[dest3][dest4] + matrix[dest4][dest1];	//2341
+	arr[10] = matrix[start][dest2] + matrix[dest2][dest4] + matrix[dest4][dest1] + matrix[dest1][dest3];	//2413
+	arr[11] = matrix[start][dest2] + matrix[dest2][dest4] + matrix[dest4][dest3] + matrix[dest3][dest1];	//2431
+																											//---여기까지 2먼저
+	arr[12] = matrix[start][dest3] + matrix[dest3][dest1] + matrix[dest1][dest2] + matrix[dest2][dest4];	//3124
+	arr[13] = matrix[start][dest3] + matrix[dest3][dest1] + matrix[dest1][dest4] + matrix[dest4][dest2];	//3142
+	arr[14] = matrix[start][dest3] + matrix[dest3][dest2] + matrix[dest2][dest1] + matrix[dest1][dest4];	//3214
+	arr[15] = matrix[start][dest3] + matrix[dest3][dest2] + matrix[dest2][dest4] + matrix[dest4][dest1];	//3241
+	arr[16] = matrix[start][dest3] + matrix[dest3][dest4] + matrix[dest4][dest1] + matrix[dest1][dest2];	//3412
+	arr[17] = matrix[start][dest3] + matrix[dest3][dest4] + matrix[dest4][dest2] + matrix[dest2][dest1];	//3421
+																											//---여기까지 3먼저
+	arr[18] = matrix[start][dest4] + matrix[dest4][dest1] + matrix[dest1][dest2] + matrix[dest2][dest3];	//4123
+	arr[19] = matrix[start][dest4] + matrix[dest4][dest1] + matrix[dest1][dest3] + matrix[dest3][dest2];	//4132
+	arr[20] = matrix[start][dest4] + matrix[dest4][dest2] + matrix[dest2][dest1] + matrix[dest1][dest3];	//4213
+	arr[21] = matrix[start][dest4] + matrix[dest4][dest2] + matrix[dest2][dest3] + matrix[dest3][dest1];	//4231
+	arr[22] = matrix[start][dest4] + matrix[dest4][dest3] + matrix[dest3][dest1] + matrix[dest1][dest2];	//4312
+	arr[23] = matrix[start][dest4] + matrix[dest4][dest3] + matrix[dest3][dest2] + matrix[dest2][dest1];	//4321
+																											//---여기까지 4 먼저
+	int index = findMin(arr, sizeof(arr) / sizeof(int));
+
+	printf("최소 거리 : %d \n", arr[index]);
+
+	return index;
+}
+
+
 void Graph::AddEdge(string start, string goal, int dist)//간선 추가
 
 {
@@ -42,7 +173,7 @@ void Graph::AddEdge(string start, string goal, int dist)//간선 추가
 
 	matrix[convertEdge(goal)][convertEdge(start)] = dist;//간선 설정
 
-	
+
 }
 
 void Graph::ViewNeighbors()const
@@ -72,11 +203,7 @@ void Graph::ViewNeighbor(int vt)const
 	for (int i = 0; i < vn; i++)
 
 	{
-
-		
-			cout << matrix[vt][i] << " ";
-		
-
+		cout << matrix[vt][i] << " ";
 	}
 
 	cout << endl;
@@ -105,6 +232,3 @@ Neighbors Graph::FindNeighbors(int vt)
 	return neighbors;
 
 }
-
-
-
--- Graph.h
+++ Graph.h
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-typedef vector<int> Neighbors;	
+typedef vector<int> Neighbors;
 
 class Graph
 
@@ -28,20 +28,54 @@ public:
 
 	void AddEdge(string start, string goal, int dist);//간선 추가
 
-	void ViewNeighbors()const;	
+	void ViewNeighbors()const;
 
 	void ViewNeighbor(int vt)const;
 
-	Neighbors FindNeighbors(int vt);	
+	Neighbors FindNeighbors(int vt);
+
+	int matrixValue(int i, int j) {
+		return matrix[i][j];
+	}
 
 	int convertEdge(string loc) {
-		
+
 		if (loc == "기숙사") return 0;
 		else if (loc == "월평역") return 1;
 		else if (loc == "유성온천역") return 2;
-		else if (loc == "대전역") return 3;
-		else if (loc == "카이스트") return 4;
-		
+		else if (loc == "카이스트") return 3;
+		else if (loc == "봉명동") return 4;
+		else if (loc == "월드컵경기장") return 5;
+		else if (loc == "복합터미널") return 6;
+		else if (loc == "은행동") return 7;
+		else if (loc == "둔산동") return 8;
+		else if (loc == "대전역") return 9;
+	}
+
+	Graph* makeGraph(string addr);
+
+	//int Graph::findMinWay(int start, Passenger* ps, Passenger* ps1);
+
+	int findMinWay(int start, int dest1, int dest2);
+
+	int findMinWay(int start, int dest1, int dest2, int dest3);
+
+	int findMinWay(int start, int dest1, int dest2, int dest3, int dest4);
+
+
+	
+
+
+	int findMin(int arr[], int size) {
+		int min = arr[0];
+		for (int i = 0; i < size; i++) {
+			if (min > arr[i]) min = arr[i];
+		}	//최소거리 찾고
+
+		for (int i = 0; i < size; i++) {
+			if (arr[i] == min) return i;	//그 인덱스 반환
+		}
+		return 0;
 	}
 
 };
