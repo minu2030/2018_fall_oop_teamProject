9596e4cef89e79ada5857ab83f997fbea76c96bd
minu2030 <minwoo518@gmail.com>
2018-12-02 21:57:54 +0900

    add

--- TeamProject/TeamProject/Calculation.cpp
+++ TeamProject/TeamProject/Calculation.cpp
@@ -5,6 +5,29 @@ int Calculation::calculate_Cost(int length) {	//총 비용 계산 함수( 1km당 900원으
 	return 900 * length;
 }
 
+string Calculation::calculate_Compare(int myLength, int howMany, Event* _event, Graph* _graph) {
+	int alone = calculate_Cost(myLength);
+	int with = calculate_Cost(calculate_totalLength(howMany, _event, _graph));
+	
+	string dest1 = NULL;
+	string dest2 = NULL;
+	string dest3 = NULL;
+	
+	switch (howMany) {
+	case 2: dest1 = _event->getPassenger(0)->getDestAddr();
+		return ("함께가는 사람들의 도착지 : " + dest1 + "\n" + "혼자갈때의 요금 : " + to_string(alone) + "\n" + "함께갈때의 요금 : " + to_string(with));
+	case 3: dest1 = _event->getPassenger(0)->getDestAddr();
+		dest2 = _event->getPassenger(1)->getDestAddr();
+		return("함께가는 사람들의 도착지 : " + dest1 + " " + dest2 + "혼자갈때의 요금 : " + to_string(alone) + "\n" + "함께갈때의 요금 : " + to_string(with));
+	case 4 : dest1 = _event->getPassenger(0)->getDestAddr();
+		dest2 = _event->getPassenger(1)->getDestAddr();
+		dest3 = _event->getPassenger(2)->getDestAddr();
+		return("함께가는 사람들의 도착지 : " + dest1 + " " + dest2 + " " + dest3 + "혼자갈때의 요금 : " + to_string(alone) + "\n" + "함께갈때의 요금 : " + to_string(with));
+	default:
+		return NULL;
+	}
+}
+
 
 int Calculation::calculate_Personal_Cost(int total, double percent) {	//개인별 내야할 돈을 계산해준다.
 	double returnValue = total * percent;	//총액 x 비율로 돈계산
@@ -25,7 +48,8 @@ int Calculation::calculate_totalLength(int n, Event* _event, Graph* _graph) {
 			_graph->originalLength(_event->getPassenger(2));
 	}
 	else if (n == 4) {
-		_graph->originalLength(_event->getPassenger(0)) + _graph->originalLength(_event->getPassenger(1)) +
+		return _graph->originalLength(_event->getPassenger(0)) + _graph->originalLength(_event->getPassenger(1)) +
 			_graph->originalLength(_event->getPassenger(2)) + _graph->originalLength(_event->getPassenger(3));
 	}
+	return 0;
 }
--- TeamProject/TeamProject/Calculation.h
+++ TeamProject/TeamProject/Calculation.h
@@ -1,8 +1,11 @@
 #ifndef CALCULATION_H
 #define CALCULATION_H
 
+
 #include "Graph.h"
 #include "Event.h"
+
+using namespace std;
 
 class Calculation {
 
@@ -12,6 +15,7 @@ class Calculation {
 		int calculate_Personal_Cost(int total, double percent);	// 개인별 내야할 돈을 계산해준다.
 		double calculate_percent(int total, int mine);	//내야할 돈의 비율계산
 		int calculate_totalLength(int n, Event* _event, Graph* _graph);	//전체 길이 계산
+		string calculate_Compare(int myLength, int howMany, Event* _event, Graph* _graph);	//함께탈때,혼자탈때 가격비교해서 string형태로 반환
 };
 
 #endif CALCULATION_H
